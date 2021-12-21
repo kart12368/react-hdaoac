196f7407cfe775508472ef30308b721ecee63231
kart12368 <patelkartu951@gmail.com>
2021-12-21 21:40:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hdaoac
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hdaoac)
---
+++ src/App.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <div className="Dash">Edvora</div>
+      <div className="Pr">Products</div>
+
+      <div className="Name">Product Name</div>
+      <hr className="one"></hr>
+      <div className="rec2">
+        <div className="box1 box">
+          <div className="but">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+              alt="image"
+            />
+          </div>
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+        <div className="box2 box">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+            alt="image"
+          />
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+        <div className="box3 box boxs">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+            alt="image"
+          />
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+        <div className="box4 box">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+            alt="image"
+          />
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+      </div>
+      <div className="Name2">Product Name</div>
+      <hr className="second"></hr>
+      <div className="rec3">
+        <div className="box5 box">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+            alt="image"
+          />
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+        <div className="box6 box">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+            alt="image"
+          />
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+        <div className="box7 box">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+            alt="image"
+          />
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+        <div className="box8 box boxsp">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2159024400&v=beta&t=CrP5Le1mWICRcaxIGNBuajHcHGFPuyNA5C8DI339lSk"
+            alt="image"
+          />
+          <p id="Locate">Location</p>
+          <p id="Des">Description</p>
+          <p id="Pro">Product Name</p>
+          <p id="Bro">Brand Name</p>
+          <p id="Dol">$</p>
+          <p id="Pri">29.99</p>
+          <p id="Date">Date:</p>
+          <p id="Da">10:12:2021</p>
+        </div>
+      </div>
+      <div className="Rec420">
+        <div className="F">Filters</div>
+        <hr className="third"></hr>
+        <div className="b B1">
+          <p className="Hi">Products</p>
+        </div>
+        <div className="b B2">
+          <p className="Hi">State</p>
+        </div>
+
+        <div className="b B3">
+          <p className="Hi">City</p>
+        </div>
+      </div>
+    </>
+  );
+}
